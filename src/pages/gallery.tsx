4db53955271ffac3a3bc24fdfe1acf530dfa8608
Jose Miguel Garcia <mikesocc@gmail.com>
2023-02-01 08:31:34 -0600

    Gallery imagenes

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,6 +1,22 @@
+import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 import MainLayout from '../components/MainLayout';
 import PaneLayout from '../components/paneLayout';
+
+const galleryImages = [
+  {
+    alt: 'Avos bg Brown',
+    path: '../images/aguacate_bg_cafe.jpg'
+  },
+  {
+    alt: 'Avos bg Dark',
+    path: '../images/aguacate_bg_obscuro.jpg'
+  },
+  {
+    alt: 'Avos two halfs',
+    path: '../images/aguacate_dos_mitades.jpg'
+  }
+];
 
 const Gallery = () => {
   return (
@@ -9,6 +25,53 @@ const Gallery = () => {
         <div className="mt-12 max-w-7xl w-full">
           <div className="h-20 bg-[#ACC465] text-white text-5xl flex justify-center items-center">
             Gallery
+          </div>
+          <div className="grid md:grid-cols-3">
+            <div>
+              <StaticImage src="../images/aguacate_bg_cafe.jpg" alt="Avos bg Brown" />
+            </div>
+            <div>
+              <StaticImage src="../images/aguacate_bg_obscuro.jpg" alt="Avos bg Dark" />
+            </div>
+            <div>
+              <StaticImage src="../images/aguacate_dos_mitades.jpg" alt="Avos two halves" className="max-h-60" />
+            </div>
+            <div>
+              <StaticImage src="../images/aguacate_hoja.jpg" alt="Avos whth leave" className="max-h-60" />
+            </div>
+            <div>
+              <StaticImage src="../images/aguacate_medio_tabla.jpg" alt="Avos table" />
+            </div>
+            <div>
+              <StaticImage src="../images/aguacate_mitad_bg_claro.jpg" alt="Avos bg white" />
+            </div>
+            <div>
+              <StaticImage src="../images/aguacate_mitad_fondo_aguacates.jpg" alt="Avos bg white" className="max-h-60" />
+            </div>
+            <div>
+              <StaticImage src="../images/aguacate_mitad.jpg" alt="Avos bg white" />
+            </div>
+            <div>
+              <StaticImage src="../images/aguacates_agua.jpg" alt="Avos bg white" />
+            </div>
+            <div>
+              <StaticImage src="../images/aguacates_arbol.jpg" alt="Avos bg white" />
+            </div>
+            <div>
+              <StaticImage src="../images/aguacates_caja_rosa.jpg" alt="Avos bg white" className="max-h-60" />
+            </div>
+            <div>
+              <StaticImage src="../images/aguacates_canasta.jpg" alt="Avos basket" />
+            </div>
+            <div>
+              <StaticImage src="../images/aguacates_fondo_azul.jpg" alt="Avos basket" />
+            </div>
+            <div>
+              <StaticImage src="../images/aguacates_guacamole.jpg" alt="Avos basket" />
+            </div>
+            <div>
+              <StaticImage src="../images/aguacates_verdes_mano.jpg" alt="Avos basket" className="max-h-60" />
+            </div>
           </div>
         </div>
       </PaneLayout>
--- src/pages/recipes.tsx
+++ src/pages/recipes.tsx
@@ -14,9 +14,9 @@ const Recipes = () => {
           <div>
             <StaticImage src="../images/aguacate_medio_tabla.jpg" alt="Avos on table" />
           </div>
-          <div className="grid grid-cols-2">
-            <div>
-              <ul>
+          <div className="grid md:grid-cols-2">
+            <div className="px-10 py-6">
+              <ul className="list-disc">
                 <li>4 large avocados</li>
                 <li>1 lb lean ground beef</li>
                 <li>4 slices of cheese of your choice</li>
@@ -48,9 +48,9 @@ const Recipes = () => {
           <div>
             <StaticImage src="../images/guacamole_aguacate.jpg" alt="Guacamole" />
           </div>
-          <div>
-            <div>
-              <ul>
+          <div className="grid md:grid-cols-2">
+            <div className="px-10 py-6">
+              <ul className="list-disc">
                 <li>1 medium avocado</li>
                 <li>1 beautiful load of rustic bread </li>
                 <li>4 slices of cheese of your choice</li>
@@ -62,9 +62,44 @@ const Recipes = () => {
                 <li>condiments of your choice</li>
               </ul>
             </div>
+            <div>
+              <StaticImage src="../images/avo_toast.jpg" alt="Avo toast" />
+            </div>
           </div>
           <div>
             <StaticImage src="../images/avocado-gd7269fd06_1920.jpg" alt="Avos tajin" />
+          </div>
+
+          <div className="grid md:grid-cols-2">
+            <div className="px-10 py-6">
+              <ul className="list-disc">
+                <li>4 large avocados</li>
+                <li>½ lb Shrimps</li>
+                <li>4 tablespoons of mayonnaise </li>
+                <li>2 boiled eggs</li>
+                <li>1 tomato</li>
+                <li>1 tablespoon chopped coriander</li>
+                <li>½ chopped onion</li>
+                <li>salt and pepper</li>
+                <li>condiments of your choice</li>
+              </ul>
+              <p>First, you need firm ripe avocados.</p>
+              <p>
+                You want to choose avocados that still feel firm all around, except at the very tip
+                (where the stem is) there should be a little give if you apply gentle pressure. If
+                you choose an avocado that is already soft all around you’ll have a really hard time
+                peeling the skin off of the avocado without having the avocado flesh sticking to the
+                skin and then your buns won’t look as nice.
+              </p>
+              <p>
+                Mix all the ingredients together and fill the avocado halves. Pour on the top some
+                sesame seeds and put the boiled egg.
+              </p>
+              <p>Enjoy this del, delicious recipe!</p>
+            </div>
+            <div>
+              <StaticImage src="../images/shrimp_egg_avo.jpg" alt="Shrimp and eggs" />
+            </div>
           </div>
           <div>
             <StaticImage src="../images/aguacates_guacamole.jpg" alt="Avos and guacamole" />
